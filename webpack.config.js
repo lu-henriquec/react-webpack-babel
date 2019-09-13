684c3df49e432bbb6aaaca5299468a7f9dff3b95
Lucas Henrique Carlos Domingos <lucaa.henrique.carlos@gmail.com>
2019-09-13 10:43:29 -0300

    setup

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+
+// old
+// const path = require('path');
+
+// new
+import path from 'path';
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public')
+  },
+  module: {
+    rules: [
+      {
+        // this is so that we can compile any React,
+        // ES6 and above into normal ES5 syntax
+        test: /\.(js|jsx)$/,
+        // we do not want anything from node_modules to be compiled
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }, {
+        test: /\.(css|scss)$/,
+        use: [
+          "style-loader", // creates style nodes from JS strings
+          "css-loader", // translates CSS into CommonJS
+          "sass-loader" // compiles Sass to CSS, using Node Sass by default
+        ]
+      }, {
+        test: /\.(jpe?g|png|gif|mp3|svg)$/i,
+        use: [
+          "file-loader", // creates style nodes from JS strings
+        ]
+      }
+    ]
+  }
+};
